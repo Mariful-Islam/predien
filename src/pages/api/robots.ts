35a7616320d143af80ea67aa034bc52ccc9b2655
mariful-nyx <mariful@heliosinsider.com>
2025-01-30 08:33:55 +0600

    fix robots

--- src/pages/api/robots.ts
+++ src/pages/api/robots.ts
@@ -4,17 +4,12 @@ import { env } from "process";
 export default function handler(req:any, res:any) {
     res.setHeader('Content-Type', 'text/plain');
 
-    const BASE_URL = env.NODE_ENV === "production" ? 'https://predien.vercel.app' : 'http://localhost:3000'
+    const BASE_URL = 'https://predien.vercel.app' 
   
     // You can modify the content dynamically based on conditions
     const robotsTxt = `
-      # Robots.txt generated dynamically for Next.js
       User-agent: *
-      Disallow: /private/
-  
-      # Allow specific user agents
-      User-agent: Googlebot
-      Disallow: /private/
+      
       Allow: /
   
       # Sitemap location
--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -2,6 +2,7 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import moment from 'moment'
+import Link from 'next/link'
 import React from 'react'
 import { SlCalender } from 'react-icons/sl'
 
@@ -21,27 +22,27 @@ function Blog({data}: {data:any}) {
 
 
   return (
-    <div className='bg-white dark:bg-black' >
+    <div className='bg-white dark:bg-black scroll-mt-12' >
       <div className='bg-gradient-to-l from-green-600 dark:from-green-800 via-violet-500 dark:via-violet-700 to-blue-400 dark:to-blue-700'>
         <Header/>
       </div>
-      <div className='max-w-[1200px] mx-auto px-4 sm:px-20 py-12 dark:text-white text-black
-      '>
-        <div className='flex gap-12'>
-          <div className='w-3/4'>
+      <div className='max-w-[1200px] mx-auto px-4 sm:px-20 py-12 dark:text-white text-black'>
+        <div className='flex flex-col-reverse md:flex-row gap-12'>
+
+          <div className='w-full md:w-3/4'>
             <h1 className='text-5xl font-bold '>{data?.title}</h1>
             <div className='flex gap-2 items-center mt-3 '><SlCalender className='stroke-1'/>{moment(data?.datetime).format('DD MMM YYYY')}</div>
             <div className='mt-3'>
-              <div dangerouslySetInnerHTML={{__html: data?.description || ''}} className='text-justify'/>
+              <div dangerouslySetInnerHTML={{__html: data?.description || ''}} className='text-justify prestyle'/>
             </div>
             
           </div>
-          <div className='w-1/4'>
+          <div className='w-full md:w-1/4'>
             <h1 className='text-2xl font-bold'>Content</h1>
             <ul>
               {h2List?.map((head, index)=>(
-                <li key={index}>
-                  {head}
+                <li key={index} className=' hover:underline font-bold'>
+                  <Link href={`#head-${index+1}`}>{head}</Link>
                 </li>
               ))}
               
